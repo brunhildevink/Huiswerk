5bcd19ad4a9af645503deaf41dfda6fcd34f45bf
brunhilde <brunhilde.vink@student.hu.nl>
2017-10-11 15:30:54 +0200

    hallo

--- Opdracht 4 versie 2/Opdracht 4.2.py
+++
@@ -1,7 +0,0 @@
-getallenlijst = [1, 2, 3, 4, 5]
-mySum = 0
-
-for num in getallenlijst:
-    mySum += num
-
-print(mySum)
--- Opdracht 4 versie 2/Opdracht 4.3.py
+++
@@ -1,8 +0,0 @@
-lang_genoeg = 120
-lengte = eval(input('hoe lang ben je?'))
-
-if lengte >= lang_genoeg:
-    print('je bent lang genoeg')
-
-else:
-    print('je bent niet lang genoeg')
